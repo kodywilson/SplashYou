559ce17b397372c056eb8befa54c00b55508887f
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-06-28 11:45:09 -0700

    dry up rspec tests

--- spec/SplashYou_spec.rb
+++ spec/SplashYou_spec.rb
@@ -1,19 +1,23 @@
 require './SplashYou.rb'
 
-describe "get_parameters" do
-  it 'reads parameters from json file' do
-    @params      = JSON.parse(File.read(File.join(File.dirname(__FILE__), "../params.json")))
-    content_type = @params['headers']['Content-Type']
-    expect(content_type).to eq 'application/x-www-form-urlencoded'
+describe 'Build html table from api data' do
+  before(:all) do
+    @params = JSON.parse(File.read(File.join(File.dirname(__FILE__), "../params.json")))
+    @vids   = Hash.new
   end
-end
 
-describe "grab_videos" do
-  it 'hits api and fills hash' do
-    @params      = JSON.parse(File.read(File.join(File.dirname(__FILE__), "../params.json")))
-    @vids = Hash.new
-    @vids = grab_videos(10)
-    title = @vids['DH_AFpnt_tU']['title']
-    expect(title).to eq 'Agave Theophilus Testimony'
+  describe "get_parameters" do
+    it 'reads parameters from json file' do
+      content_type = @params['headers']['Content-Type']
+      expect(content_type).to eq 'application/x-www-form-urlencoded'
+    end
+  end
+
+  describe "grab_videos" do
+    it 'hits api and fills hash' do
+      @vids = grab_videos(10)
+      title = @vids['DH_AFpnt_tU']['title']
+      expect(title).to eq 'Agave Theophilus Testimony'
+    end
   end
 end
